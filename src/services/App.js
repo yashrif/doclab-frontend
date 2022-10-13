80e3f69e9e808a1c1a27499c525c41cae53ec599
Mansibsadh <mansibsadh48@gmail.com>
2022-10-14 00:43:25 +0600

    Added services page

---
+++ src/services/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Content></Content>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
