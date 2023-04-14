eb318561325f22b254e9296ec8be8eda3e3fc962
Yashrif Arifin <yaswoccho@gmail.com>
2023-04-14 22:17:20 +0600

    🎨 style(README.md): update copyright symbol The copyright symbol is now updated to use the HTML entity &copy; instead of the word "Copyright".
    🎨 style(MainContainer.jsx): refactor MainContainer to use Chakra UI Grid component
    The MainContainer component is now refactored to use the Chakra UI Grid component instead of a div with inline styles. This improves readability and maintainability of the code.
    
    🎨 style(PageSummary.jsx): refactor PageSummary to use Chakra UI components
    The PageSummary component is now refactored to use Chakra UI components instead of divs with inline styles. This improves readability and maintainability of the code. The fade-out animation is now defined using the Chakra UI keyframes utility.
    
    🎨 style(PersonCard.jsx): refactor PersonCard component to use Chakra UI components
    The PersonCard component has been refactored to use Chakra UI components instead of plain HTML and CSS. This improves consistency with the rest of the application and makes it easier to maintain and update the styling.
    
    🎨 style(SearchContainer.jsx): refactor SearchContainer component to use Chakra UI components
    The SearchContainer component has been refactored to use Chakra UI components instead of plain HTML and CSS. This improves consistency with the rest of the application and makes it easier to maintain and update the styling.

--- src/findDoctor/MainContainer.jsx
+++ src/findDoctor/MainContainer.jsx
@@ -1,25 +1,22 @@
 import React from "react";
+import { Grid } from "@chakra-ui/react";
+
 import theme from "../styling/theme.jsx";
 
 const MainContainer = ({ children }) => {
   return (
-    <section
-      style={{
-        height: `calc(
-            100% - ${theme.typography.containerHeight.header} - ${theme.typography.sectionGap.medium} - ${theme.typography.sectionGap.headerBottom} + 2.4rem
-          )`,
-        padding: `1.6rem ${theme.typography.sectionGap.large} ${theme.typography.sectionGap.medium}`,
-        display: "grid",
-        gridTemplateColumns: `minmax(${theme.typography.containerWidth.overviewMin}, 4fr) 13fr`,
-        alignItems: "start",
-        columnGap: `${theme.typography.sectionGap.medium}`,
-        // overflow: "hidden",
-        backgroundColor: `${theme.colors.bgContainer}`,
-        overflowY: "hidden",
-      }}
+    <Grid
+      as="section"
+      height={`calc(100% - ${theme.typography.containerHeight.header} - ${theme.typography.sectionGap.medium} - ${theme.typography.sectionGap.headerBottom} + 2.4rem)`}
+      padding={`1.6rem ${theme.typography.sectionGap.large} ${theme.typography.sectionGap.medium}`}
+      gridTemplateColumns={`minmax(${theme.typography.containerWidth.overviewMin}, 4fr) 13fr`}
+      alignItems="start"
+      columnGap={`${theme.typography.sectionGap.medium}`}
+      backgroundColor={`${theme.colors.bgContainer}`}
+      overflowY="hidden"
     >
       {children.map((child) => child)}
-    </section>
+    </Grid>
   );
 };
 
--- src/findDoctor/pageOverview/SearchContainer.jsx
+++ src/findDoctor/pageOverview/SearchContainer.jsx
@@ -1,53 +1,49 @@
 import React from "react";
+import { Box, Flex, Text } from "@chakra-ui/react";
+
 import Search from "../../reusable/Search.jsx";
 import theme from "../../styling/theme.jsx";
 
 const SearchContainer = ({ term, setTerm, category }) => {
   return (
     <>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          gap: "1.2rem",
-          marginBottom: "1.2rem",
-        }}
+      <Flex
+        justifyContent="space-between"
+        alignItems="center"
+        gap="1.2rem"
+        marginBottom="1.2rem"
       >
-        <h3
-          style={{
-            fontSize: "1.5rem",
-            lineHeight: "1.5",
-            fontWeight: "600",
-            color: `${theme.typography.colors.primaryFirst.primary}`,
-          }}
+        <Text
+          fontSize="1.5rem"
+          lineHeight="1.5"
+          fontWeight="600"
+          color={`${theme.typography.colors.primaryFirst.primary}`}
         >
           {[category.slice(0, 1).toUpperCase(), category.slice(1)].join("")}
-        </h3>
-        <div
-          style={{
-            fontSize: "1.5rem",
-            lineHeight: "1.5",
-            display: "flex",
-            alignItems: "center",
-            gap: "0.8rem",
-          }}
+        </Text>
+        <Flex
+          fontSize="1.5rem"
+          lineHeight="1.5"
+          alignItems="center"
+          gap="0.8rem"
         >
-          <p className="search-container-filter-title">
+          <Text>
             View:{" "}
-            <span style={{ color: "#333", fontWeight: "500" }}>Gender</span>{" "}
-          </p>
+            <Text as={"span"} style={{ color: "#333", fontWeight: "500" }}>
+              Gender
+            </Text>{" "}
+          </Text>
           <ion-icon name="chevron-down-outline"></ion-icon>
-        </div>
-      </div>
-      <div className="search-bar">
+        </Flex>
+      </Flex>
+      <Box>
         <Search
           term={term}
           setTerm={setTerm}
           category={category}
           bg="#e9ecef"
         />
-      </div>
+      </Box>
     </>
   );
 };
